933bdb3d65002c7eb21c75256a0ddb0b2a980d11
FIRMAN PANJI <fpu2468@gmail.com>
2023-12-23 01:08:27 +0700

    feat: Finished enqueuing stylesheets and scripts for Wordpress best practices to avoid conflicts between styles and scripts, and making sure they are loaded correctly by order

--- front-page.php
+++ front-page.php
@@ -33,7 +33,7 @@
             </button>
 
             <div id="navigation" class="collapse navbar-collapse flex-column">
-                <img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo">
+                <img class="mb-3 mx-auto logo" src="assets/images/logo.png" alt="logo">
 
                 <ul class="navbar-nav flex-column text-sm-center text-md-left">
                     <li class="nav-item active">
@@ -76,13 +76,13 @@
                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="images/slider2.jpg" class="d-block w-100" alt="1">
+                            <img src="/assets/images/slider1.jpg" class="d-block w-100" alt="1">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/slider1.jpg" class="d-block w-100" alt="2">
+                            <img src="/assets/images/slider2.jpg" class="d-block w-100" alt="2">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/slider3.jpg" class="d-block w-100" alt="3">
+                            <img src="assets/images/slider3.jpg" class="d-block w-100" alt="3">
                         </div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -131,12 +131,8 @@
 
     </div>
 
-
+    <?php wp_footer() ?>
     <!-- Bootstrap Javascript -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
 
 </body>
 
--- functions.php
+++ functions.php
@@ -14,9 +14,10 @@ add_action('wp_enqueue_scripts', 'templatepanji_register_styles');
 
 function templatepanji_register_scripts()
 {
-    
+    wp_enqueue_script('templatepanji-jquery', "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), '3.4.1', true);
+    wp_enqueue_script('templatepanji-popper', "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), '3.4.1', true);
+    wp_enqueue_script('templatepanji-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), '3.4.1', true);
+    wp_enqueue_script('templatepanji-main', get_template_directory_uri() . "/assets/js/main.js", array(), '3.4.1', true);
 }
 
 add_action('wp_enqueue_scripts', 'templatepanji_register_scripts');
-
-?>
